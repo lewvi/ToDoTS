b5e370614a9f0410fa9b5d4a31a1721fea65882b
lewvi <lew_rip@hotmail.com>
2022-05-06 13:00:05 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-
 import InputForm from './component/inputForm';
+import TodoList from './component/TodoList';
 import { Todo } from './models/model';
 
 
 const App: React.FC = () => {
 
   const [todo, setTodo] = useState<string>("");
+
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
@@ -19,11 +20,13 @@ const App: React.FC = () => {
     }
   };
 
+  console.log(todos)
 
   return (
     <div className="App">
       <span className="App-header">To Do List</span>
-        <InputForm todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <InputForm todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
--- src/component/inputForm.tsx
+++ src/component/inputForm.tsx
@@ -2,9 +2,9 @@ import React, { useRef } from "react";
 import "./styles.css";
 
 interface props {
-    todo : string;
-    setTodo : React.Dispatch<React.SetStateAction<string>>;
-    handleAdd : (e: React.FormEvent) => void;
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
 }
 
 
@@ -13,26 +13,24 @@ const InputForm: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
     const inputRef = useRef<HTMLInputElement>(null);
 
     return (
-        <div>
-            <form
-                className="input"
-                onSubmit={(e) => {
-                    handleAdd(e);
-                    inputRef.current?.blur();
-                }}>
-                <input
-                    type="text"
-                    placeholder="Enter...."
-                    value={todo}
-                    ref={inputRef}
-                    onChange={(e) => setTodo(e.target.value)}
-                    className="input-box"
-                />
-                <button type="submit" className="submit-button">
-                    Save
-                </button>
-            </form>
-        </div>
+        <form
+            className="input"
+            onSubmit={(e) => {
+                handleAdd(e);
+                inputRef.current?.blur();
+            }}>
+            <input
+                type="text"
+                placeholder="Enter...."
+                value={todo}
+                ref={inputRef}
+                onChange={(e) => setTodo(e.target.value)}
+                className="input-box"
+            />
+            <button type="submit" className="submit-button">
+                Save
+            </button>
+        </form>
     )
 }
 
